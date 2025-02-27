4d94eafce82243b4196dcd87269b070da68966a0
DumbestPerson224 <eTmullall@outlook.com>
2025-02-26 22:26:02 -0500

    Fixed bug

--- AimAssist.cpp
+++ AimAssist.cpp
@@ -18,9 +18,9 @@ public:
 		}
 	}
 
-	void decrease_strength(float amount) {
-		if (isAimAssistOn() && amount <= strength) {
-			strength -= amount;
+	void decrease_strength(float scopeX, float scopeY, float scopeZ, float scopeSpeed, float opponentX, float opponentY, float opponentZ, float opponentDistance, float opponentSpeed) {
+		if (isAimAssistOn()) {
+			strength -= (std::pow(std::fabs(scopeX - opponentX) + std::fabs(scopeY - opponentY - 1) + std::fabs(scopeZ - opponentZ - 1) + std::fabs(opponentSpeed - scopeSpeed) / opponentDistance, 2));
 		}
 	}
 
@@ -37,12 +37,12 @@ public:
 class LinearTrackingAimAssist : public ExponentialTrackingAimAssist {
 public:
 	void increase_strength(float scopeX, float scopeY, float scopeZ, float scopeSpeed, float opponentX, float opponentY, float opponentZ, float opponentSpeed, float opponentDistance) {
-		strength += std::fabs(scopeX - opponentX) + std::fabs(scopeY - opponentY) + std::fabs(scopeZ - opponentZ) + std::(scopeSpeed - opponentSpeed) / opponentDistance;
+		strength += std::fabs(scopeX - opponentX) + std::fabs(scopeY - opponentY) + std::fabs(scopeZ - opponentZ) + std::fabs(scopeSpeed - opponentSpeed) / opponentDistance;
 	}
 
-	void decease_strength(float amount) {
-		if (isAimAssistOn() && amount <= strength) {
-			strength -= amount;
+	void decease_strength(float scopeX, float scopeY, float scopeZ, float scopeSpeed, float opponentX, float opponentY, float opponentZ, float opponentDistance, float opponentSpeed) {
+		if (isAimAssistOn()) {
+			strength -= (std::pow(std::fabs(scopeX - opponentX) + std::fabs(scopeY + opponentY) + std::fabs(scopeZ - opponentZ) + std::fabs(opponentSpeed - scopeSpeed) / opponentDistance, 2));
 		}
 	}
 };
